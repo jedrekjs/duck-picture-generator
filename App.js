b64a2ab2204eac7d2024adc143a291a95f89da18
Jędrzej Smolarkiewicz <jedrzej.smolarkiewicz@gmail.com>
2022-10-20 19:44:13 +0200

    added loader, fixed layout

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-var imgContainer = document.getElementById("duck-img");
+const imgContainer = document.getElementById("duck-img");
+const toggleButton = document.getElementById("toggle-button");
+const loader = document.getElementById("loader-container");
+
 async function getImg(){
     return await fetch("https://thawing-crag-96552.herokuapp.com/https://random-d.uk/api/random")
         .then((res) => res.json())
@@ -6,8 +9,15 @@ async function getImg(){
 }
 
 async function changeImg(){
+    toggleButton.disabled = true;
+    imgContainer.style.opacity = 0.2;
+    loader.style.display = "block";
     const imgSrc = await getImg();
     imgContainer.src = imgSrc;
+    toggleButton.disabled = false;
+    imgContainer.style.opacity = 1;
+    loader.style.display = "none";
 }
+
 
 window.onload = changeImg();
